f5b604f9edc8cb97b93b44dc7ad8d985311d8ca9
Jennifer Hurd <jenhurd8@gmail.com>
2018-08-07 12:12:47 -0500

    reviewReducer updated

--- src/ducks/reviewReducer.js
+++ src/ducks/reviewReducer.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 //constants
 const GET_REVIEWS = "GET_REVIEWS";
+const DELETE_REVIEW = "DELETE_REVIEW";
+const UPDATE_REVIEW = "UPDATE_REVIEW";
 
 //action creators
 export function getReviews() {
@@ -10,6 +12,21 @@ export function getReviews() {
     payload: axios.get("/api/reviews")
   };
 }
+
+export function deleteReview(id) {
+  return {
+    type: DELETE_REVIEW,
+    payload: axios.delete(`/api/review/${id}`)
+  };
+}
+
+export function updateReview(id, obj) {
+  return {
+    type: UPDATE_REVIEW,
+    payload: axios.put(`/api/review/${id}`, obj)
+  };
+}
+
 //initial state
 const initialState = {
   reviews: [],
@@ -35,6 +52,42 @@ export default function reviewReducer(state = initialState, action) {
         ...state,
         isLoading: false,
         reviews: action.payload.data
+      };
+
+    case `${DELETE_REVIEW}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${DELETE_REVIEW}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        reviews: action.payload.data
+      };
+    case `${DELETE_REVIEW}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case `${UPDATE_REVIEW}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${UPDATE_REVIEW}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        reviews: action.payload.data
+      };
+    case `${UPDATE_REVIEW}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
       };
 
     default:
