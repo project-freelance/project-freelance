831da2fc296202a68c10b99ef20a059f8fc6eed0
truongtony9 <truong99t@gmail.com>
2018-08-07 15:11:06 -0500

    initial profile update

--- src/components/Main/Profile/Profile.js
+++ src/components/Main/Profile/Profile.js
@@ -1,8 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 class Profile extends Component {
   render() {
-    return <div>Profile</div>;
+    return (
+      <div>
+        <div className="employer_profile_title">
+          <TextField
+            label="Employer Title"
+            id="simple-start-adornment"
+            className=""
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Kg</InputAdornment>
+              )
+            }}
+          />
+        </div>
+        <div className="employer_profile_image">Company Logo</div>
+        <div className="employer_profile_name">First Name and Last Name</div>
+        <div className="employer_profile_city">City</div>
+        <div className="employer_profile_upload_company_logo">
+          Upload Company Logo
+        </div>
+        <div className="employer_profile_about">About</div>
+      </div>
+    );
   }
 }
 
