e936c09367318f780df3a466c6b0cf55bd523d65
Jennifer Hurd <jenhurd8@gmail.com>
2018-08-07 11:53:37 -0500

    employer reducer setup

--- src/ducks/employerReducer.js
+++ src/ducks/employerReducer.js
@@ -5,6 +5,11 @@ const GET_EMPLOYER = "GET_EMPLOYER";
 const GET_EMPLOYERS = "GET_EMPLOYERS";
 const UPDATE_EMPLOYER = "UPDATE_EMPLOYER";
 
+const ADD_EMPLOYER_POST = "ADD_EMPLOYER_POST";
+const GET_EMPLOYER_POSTS = "GET_EMPLOYER_POSTS";
+const DELETE_EMPLOYER_POST = "DELETE_EMPLOYER_POST";
+const UPDATE_EMPLOYER_POST = "UPDATE_EMPLOYER_POST";
+
 export function addEmployer(
   bio,
   company,
@@ -61,10 +66,45 @@ export function updateEmployer(
   };
 }
 
+export function addEmployerPost(title, body, specialty, price, user_id) {
+  return {
+    type: ADD_EMPLOYER_POST,
+    payload: axios.post("/api/employerPost/", {
+      title,
+      body,
+      specialty,
+      price,
+      user_id
+    })
+  };
+}
+
+export function getEmployerPosts() {
+  return {
+    type: GET_EMPLOYER_POSTS,
+    payload: axios.get(`/api/employerPosts/`)
+  };
+}
+
+export function deleteEmployerPost(id) {
+  return {
+    type: DELETE_EMPLOYER_POST,
+    payload: axios.delete(`/api/employerPost/${id}`)
+  };
+}
+
+export function updateEmployerPost(id, obj) {
+  return {
+    type: UPDATE_EMPLOYER_POST,
+    payload: axios.put(`/api/employerPost/${id}`, obj)
+  };
+}
+
 //initial state
 const initialState = {
   employer: [],
   employers: [],
+  employerPosts: [],
   isLoading: false,
   error: ""
 };
@@ -141,5 +181,80 @@ export default function employerReducer(state = initialState, action) {
         isLoading: false,
         error: action.payload
       };
+
+    case `${ADD_EMPLOYER_POST}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${ADD_EMPLOYER_POST}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        employerPosts: action.payload.data
+      };
+    case `${ADD_EMPLOYER_POST}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case `${GET_EMPLOYER_POSTS}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${GET_EMPLOYER_POSTS}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        employerPosts: action.payload.data
+      };
+    case `${GET_EMPLOYER_POSTS}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case `${DELETE_EMPLOYER_POST}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${DELETE_EMPLOYER_POST}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        employerPosts: action.payload.data
+      };
+    case `${DELETE_EMPLOYER_POST}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    case `${UPDATE_EMPLOYER_POST}_PENDING`:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case `${UPDATE_EMPLOYER_POST}_FULFILLED`:
+      return {
+        ...state,
+        isLoading: false,
+        employerPosts: action.payload.data
+      };
+    case `${UPDATE_EMPLOYER_POST}_REJECTED`:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+
+    default:
+      return state;
   }
 }
