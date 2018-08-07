9eb85d81a42995212ac2fc5c73a36a30acdbbe46
Jennifer Hurd <jenhurd8@gmail.com>
2018-08-07 14:38:48 -0500

    updated Setup

--- src/components/Setup/Setup.js
+++ src/components/Setup/Setup.js
@@ -1,18 +1,50 @@
 import React, { Component } from "react";
 
 class Setup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      role: "",
+      specialty: ""
+    };
+  }
+
+  chooseEmployer() {
+    this.setState({
+      role: "Employer"
+    });
+  }
+
+  chooseFreelancer() {
+    this.setState({
+      role: "Freelancer"
+    });
+  }
+
+  chooseDeveloper() {
+    this.setState({
+      role: "Developer"
+    });
+  }
+
+  chooseDesigner() {
+    this.setState({
+      role: "Designer"
+    });
+  }
+
   render() {
     return (
       <div>
         <div>Are you an Employer or a Freelancer</div>
         <div>
-          <button onClick={() => this.props.updateRole}>Employer</button>
-          <button onClick={() => this.props.updateRole}>Freelancer</button>
+          <button onClick={() => this.chooseEmployer}>Employer</button>
+          <button onClick={() => this.chooseFreelancer}>Freelancer</button>
         </div>
         <div>
           Are you a Developer or a Designer?
-          <button onClick={() => this.props.updateSpecialty}>Employer</button>
-          <button onClick={() => this.props.updateSpecialty}>Freelancer</button>
+          <button onClick={() => this.chooseDeveloper}>Developer</button>
+          <button onClick={() => this.chooseDesigner}>Designer</button>
         </div>
       </div>
     );
