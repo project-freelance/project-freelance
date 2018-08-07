72b171fe1c5fec5da564069923c13b8477f147ab
Dylan Brandenburg <brandenburgd@Gmail.com>
2018-08-07 14:10:32 -0500

    added cssGrid + settings page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
+import "./Main.css";
 
 import Nav from "../Main/Nav/Nav";
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="main__container">
+        <div className="main__topNav">Main</div>
+        <div className="main__sideNav">
           <Nav />
         </div>
-        <div>{this.props.children}</div>
-        Main
+        <div className="main__propsContainer">{this.props.children}</div>
       </div>
     );
   }
