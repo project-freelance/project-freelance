9a6c617ddd5a44a0649fa4158a6ccdd77da046d5
Dylan Brandenburg <brandenburgd@Gmail.com>
2018-08-07 11:02:47 -0500

    setup routes & components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-// import store from "./redux/store";
+import store from "./store";
 import routes from "./routes";
 
 class App extends Component {
   render() {
     return (
-      // <Provider store={store}>
-      <HashRouter>
-        <div className="App">
-          <div>{routes}</div>
-        </div>
-      </HashRouter>
-      // </Provider>
+      <Provider store={store}>
+        <HashRouter>
+          <div className="App">
+            <div>{routes}</div>
+          </div>
+        </HashRouter>
+      </Provider>
     );
   }
 }
