b77730814ad3049da2bc0e9dc6a335f68978d524
truongtony9 <truong99t@gmail.com>
2018-08-08 09:06:37 -0500

    post update

--- src/components/Main/Feed/Post/Post.js
+++ src/components/Main/Feed/Post/Post.js
@@ -1,8 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addFreelancerPost } from '../../../../ducks/freelancerReducer';
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
   render() {
-    return <div>Post</div>;
+    return (
+      <div>
+        <div>Create Freelancer Posting</div>
+        <div>Post Title:</div>
+        <div>Post Description:</div>
+        <button>Cancel</button>
+        <button>Submit</button>
+      </div>
+    );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Main from "./components/Main/Main";
-import Splash from "./components/Splash/Splash";
-import Feed from "./components/Main/Feed/Feed";
-import Profile from "./components/Main/Profile/Profile";
-import Setup from "./components/Setup/Setup";
-import Settings from "./components/Main/Settings/Settings";
+import Main from './components/Main/Main';
+import Splash from './components/Splash/Splash';
+import Feed from './components/Main/Feed/Feed';
+import Profile from './components/Main/Profile/Profile';
+import Setup from './components/Setup/Setup';
+import Settings from './components/Main/Settings/Settings';
+import Post from './components/Main/Feed/Post/Post';
 
 export default (
   <Switch>
@@ -22,6 +23,7 @@ export default (
               <Route path="/main/feed" component={Feed} />
               <Route path="/main/profile" component={Profile} />
               <Route path="/main/settings" component={Settings} />
+              <Route path="/main/feed/post" component={Post} />
             </Switch>
           </Main>
         );
