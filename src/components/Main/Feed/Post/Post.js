344ebfc859db675a4bc7f821a7856fcd9528450d
truongtony9 <truong99t@gmail.com>
2018-08-08 14:22:43 -0500

    posting update

--- src/components/Main/Feed/Post/Post.js
+++ src/components/Main/Feed/Post/Post.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { addFreelancerPost } from '../../../../ducks/freelancerReducer';
 import { addEmployerPost } from '../../../../ducks/employerReducer';
 import { getUser } from '../../../../ducks/userReducer';
+import TextField from '@material-ui/core/TextField';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
 
 class Post extends Component {
   constructor(props) {
@@ -10,23 +14,28 @@ class Post extends Component {
     this.state = {
       title: '',
       body: '',
-      specialty: '',
-      price: 0
+      specialty: ''
     };
   }
 
-  onChangeHandlerFreelancer = e => {
-    console.log(`${e.target.name}: `, e.target.value);
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+  // onChangeHandlerFreelancer = e => {
+  //   console.log(`${e.target.name}: `, e.target.value);
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   });
+  // };
 
-  onChangeHandlerEmployer = ev => {
-    console.log(`${ev.target.name}: `, ev.target.value);
+  // onChangeHandlerEmployer = ev => {
+  //   console.log(`${ev.target.name}: `, ev.target.value);
+  //   this.setState({
+  //     [ev.target.name]: ev.target.value
+  //   });
+  // };
+  handleSpecialtyChange = specialty => event => {
     this.setState({
-      [ev.target.name]: ev.target.value
+      [specialty]: event.target.value
     });
+    console.log(this.state);
   };
 
   render() {
@@ -39,24 +48,23 @@ class Post extends Component {
         <div className="freelancer__posting">
           <h2>Create Freelancer Posting</h2>
           <form>
-            <div>
-              Title:
-              <input
-                onChange={this.onChangeHandlerFreelancer}
-                name="title"
-                value={title}
-                placeholder="title..."
-              />
-            </div>
-            <div>
-              Description:
-              <input
-                onChange={this.onChangeHandlerFreelancer}
-                name="body"
-                value={body}
-                placeholder="description..."
-              />
-            </div>
+            <TextField
+              id="Title"
+              label="Title"
+              className={'freelancer__post__title__input'}
+              value={title}
+              onChange={e => this.setState({ title: e.target.value })}
+              margin="normal"
+            />
+
+            <TextField
+              id="Description"
+              label="Description"
+              className={'freelancer__post__body__input'}
+              value={body}
+              onChange={e => this.setState({ body: e.target.value })}
+              margin="normal"
+            />
 
             <button
               onClick={() => {
@@ -77,42 +85,76 @@ class Post extends Component {
         <div className="employer__posting">
           <h2>Create Employer Posting</h2>
           <form>
-            <div>
-              Title:
-              <input
-                onChange={this.onChangeHandlerEmployer}
-                name="title"
-                value={title}
-                placeholder="title..."
-              />
-            </div>
-            <div>
-              Description:
-              <input
-                onChange={this.onChangeHandlerEmployer}
-                name="body"
-                value={body}
-                placeholder="description..."
-              />
-            </div>
-            <div>
-              Specialty:
-              <input
-                onChange={this.onChangeHandlerEmployer}
-                name="specialty"
+            <TextField
+              id="Title"
+              label="Title"
+              className={'employer__post__title__input'}
+              value={title}
+              onChange={e => this.setState({ title: e.target.value })}
+              margin="normal"
+            />
+
+            <TextField
+              id="Description"
+              label="Description"
+              className={'employer__post__body__input'}
+              value={body}
+              onChange={e => this.setState({ body: e.target.value })}
+              margin="normal"
+            />
+
+            {/* <TextField
+              id="Description"
+              label="Specialty"
+              className={'employer__post__specialty__input'}
+              value={specialty}
+              onChange={e => this.setState({ specialty: e.target.value })}
+              margin="normal"
+            /> */}
+
+            <FormControl className={'form'}>
+              <NativeSelect
+                className={'employer__specialty__input'}
                 value={specialty}
-                placeholder="Developer or Designer..."
-              />
-            </div>
-            <div>
-              Price:
-              <input
-                onChange={this.onChangeHandlerEmployer}
-                name="price"
-                value={price}
-                placeholder="price..."
-              />
-            </div>
+                name="specialty"
+                onChange={this.handleSpecialtyChange('specialty')}
+              >
+                <option value="" disabled>
+                  Specialty
+                </option>
+                <option value={0}> Developer </option>
+                <option value={1}> Designer </option>
+              </NativeSelect>
+              <FormHelperText>Choose one</FormHelperText>
+            </FormControl>
+
+            <TextField
+              id="Description"
+              label="Price"
+              className={'employer__post__price__input'}
+              value={price}
+              onChange={e => this.setState({ price: e.target.value })}
+              margin="normal"
+            />
+
+            {/* <FormControl className={'form'}>
+              <NativeSelect
+                className={'settings__experience__input'}
+                value={experience}
+                name="experience"
+                onChange={this.handleExperienceChange('experience')}
+              >
+                <option value="" disabled>
+                  experience
+                </option>
+                <option value={0}> 0 years</option>
+                <option value={1}> >1 year</option>
+                <option value={3}>1 - 3 years</option>
+                <option value={5}> >5 years</option>
+              </NativeSelect>
+              <FormHelperText>Experience</FormHelperText>
+            </FormControl> */}
+
             <button
               onClick={() => {
                 this.props.addEmployerPost(
