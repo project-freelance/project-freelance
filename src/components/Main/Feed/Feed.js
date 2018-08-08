0555d170c91ac1af5fdd9a7d966ced187dbdf9e6
Jennifer Hurd <jenhurd8@gmail.com>
2018-08-08 11:41:43 -0500

    feed updated

--- src/components/Main/Feed/Feed.js
+++ src/components/Main/Feed/Feed.js
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { getEmployerPosts } from "../../../ducks/employerReducer";
-import { getFreelancerPosts } from "../../../ducks/freelancerReducer";
-import { post } from "../../../../node_modules/request";
+import { getEmployerPosts, getEmployers } from "../../../ducks/employerReducer";
+import {
+  getFreelancerPosts,
+  getFreelancers
+} from "../../../ducks/freelancerReducer";
 import "../Feed/Feed.css";
 
 class Feed extends Component {
   componentDidMount() {
     this.props.getEmployerPosts();
     this.props.getFreelancerPosts();
+    this.props.getEmployers();
+    this.props.getFreelancers();
   }
 
   render() {
-    console.log(this.props);
-
-    let { employerPosts, freelancerPosts, isLoading } = this.props;
-    console.log(freelancerPosts);
+    console.log(freelancers);
+    let {
+      employerPosts,
+      freelancerPosts,
+      isLoading,
+      employers,
+      freelancers
+    } = this.props;
 
     freelancerPosts = isLoading ? (
       <p>Loading...</p>
     ) : (
       freelancerPosts.map((post, i) => {
-        console.log(post);
+        console.log(post.user_id);
+        console.log(freelancers);
         return (
           <div className="feed__freelancerPostContainer" key={i}>
-            <p>{post.title}</p>
-            <p>{post.body}</p>
+            <div className="freelancerProfile">
+              <div>Image</div>
+            </div>
+            <div className="feed__freelancerPosting">
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+              <button>More Info</button>
+            </div>
           </div>
         );
       })
@@ -35,20 +50,20 @@ class Feed extends Component {
       <p>Loading...</p>
     ) : (
       employerPosts.map((post, i) => {
-        console.log(post);
         return (
           <div className="feed__employerPostContainer" key={i}>
             <p>{post.title}</p>
             <p>{post.body}</p>
             <p>{post.specialty}</p>
             <p>{post.price}</p>
+            <button>More Info</button>
           </div>
         );
       })
     );
 
     return (
-      <div>
+      <div className="feed__container">
         <h1>In the Feed...</h1>
         {freelancerPosts}
         {employerPosts}
@@ -60,11 +75,13 @@ class Feed extends Component {
 function mapStateToProps(state) {
   return {
     employerPosts: state.employerReducer.employerPosts,
-    freelancerPosts: state.freelancerReducer.freelancerPosts
+    freelancerPosts: state.freelancerReducer.freelancerPosts,
+    employers: state.employerReducer.employers,
+    freelancers: state.freelancerReducer.freelancers
   };
 }
 
 export default connect(
   mapStateToProps,
-  { getEmployerPosts, getFreelancerPosts }
+  { getEmployerPosts, getFreelancerPosts, getEmployers, getFreelancers }
 )(Feed);
