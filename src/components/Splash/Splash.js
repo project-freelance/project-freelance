7341bf363ebfb264fe734c211362ad74c66e6d29
truongtony9 <truong99t@gmail.com>
2018-08-09 13:46:31 -0500

    starting splash page

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Splash extends Component {
   render() {
     return (
       <div>
-        Splash
-        <a href="http://localhost:3001/login">Login</a>
+        <div className="splash__logo__info__page">Splash</div>
+        <div className="splash__login">
+          <a href="http://localhost:3001/login">Login</a>
+        </div>
+        <div className="splash__right__image__top">Test.</div>
+        <div className="splash__right__image__middle">
+          <a href="http://via.placeholder.com/350x150" />
+        </div>
+        <div className="splash__right__image__bottom">
+          <a href="https://placeimg.com/640/480/tech" />
+        </div>
+        <div className="splash__left__image__top">
+          <a href="https://placeimg.com/640/480/tech" />
+        </div>
+
+        <div className="splash__left__image__middle">
+          <a href="https://placeimg.com/640/480/tech" />
+        </div>
+        <div className="splash__left__image__bottom">
+          <a href="https://placeimg.com/640/480/tech" />
+        </div>
       </div>
     );
   }
