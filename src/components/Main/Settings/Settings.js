719b642e59ce0cf5e3b7ca805f78adb05a1850e5
truongtony9 <truong99t@gmail.com>
2018-08-09 14:59:11 -0500

    update

--- src/components/Main/Settings/Settings.js
+++ src/components/Main/Settings/Settings.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getUser } from "../../../ducks/userReducer";
-import ReactS3Uploader from "react-s3-uploader";
-import { updateUser } from "../../../ducks/userReducer";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getUser } from '../../../ducks/userReducer';
+import ReactS3Uploader from 'react-s3-uploader';
+import { updateUser } from '../../../ducks/userReducer';
 // Material UI
 
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Button from "@material-ui/core/Button";
-import NativeSelect from "@material-ui/core/NativeSelect";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import TextField from '@material-ui/core/TextField';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import LinearProgress from '@material-ui/core/LinearProgress';
 // Material UI
 
-import "./Settings.css";
+import './Settings.css';
 
 class Settings extends Component {
   constructor(props) {
@@ -23,11 +23,11 @@ class Settings extends Component {
       last_name: this.props.user[0].last_name,
       email: this.props.user[0].email,
       experience: 0,
-      city: "",
+      city: '',
       profile_image: this.props.user[0].profile_image,
       completed: 0,
       percent: 0,
-      bio: ""
+      bio: ''
     };
   }
   handleChange = name => event => {
@@ -91,7 +91,7 @@ class Settings extends Component {
           />
           <h2>
             {first_name}
-            {"  "} {last_name}
+            {'  '} {last_name}
           </h2>
         </div>
         <div className="settings__bio">
@@ -110,7 +110,7 @@ class Settings extends Component {
         </div>
         <div className="settings__info">
           <form
-            className={"settings__formContainer"}
+            className={'settings__formContainer'}
             noValidate
             autoComplete="off"
           >
@@ -119,7 +119,7 @@ class Settings extends Component {
                 id="First Name"
                 label="First Name"
                 fullWidth
-                className={"settings__firstName__input"}
+                className={'settings__firstName__input'}
                 value={first_name}
                 onChange={e => this.setState({ first_name: e.target.value })}
                 margin="normal"
@@ -130,7 +130,7 @@ class Settings extends Component {
                 id="Last Name"
                 label="Last Name"
                 fullWidth
-                className={"settings__lastname__input"}
+                className={'settings__lastname__input'}
                 value={last_name}
                 onChange={e => this.setState({ last_name: e.target.value })}
                 margin="normal"
@@ -141,7 +141,7 @@ class Settings extends Component {
                 id="name"
                 label="email"
                 fullWidth
-                className={"settings__email__input"}
+                className={'settings__email__input'}
                 value={email}
                 onChange={e => this.setState({ email: e.target.value })}
                 margin="normal"
@@ -152,19 +152,19 @@ class Settings extends Component {
                 id="name"
                 label="city"
                 fullWidth
-                className={"settings__city__input"}
+                className={'settings__city__input'}
                 value={city}
                 onChange={e => this.setState({ city: e.target.value })}
                 margin="normal"
               />
             </div>
             <div>
-              <FormControl className={"form"}>
+              <FormControl className={'form'}>
                 <NativeSelect
-                  className={"settings__experience__input"}
+                  className={'settings__experience__input'}
                   value={experience}
                   name="experience"
-                  onChange={this.handleExperienceChange("experience")}
+                  onChange={this.handleExperienceChange('experience')}
                 >
                   <option value="" disabled>
                     experience
@@ -188,7 +188,7 @@ class Settings extends Component {
             onProgress={this.progress}
             onFinish={this.onPictureUpload}
             contentDisposition="auto"
-            scrubFilename={filename => filename.replace(/[^\w\d_\-.]+/gi, "")}
+            scrubFilename={filename => filename.replace(/[^\w\d_\-.]+/gi, '')}
             inputRef={cmp => (this.uploadInput = cmp)}
             server={process.env.REACT_APP_DEV_HOST}
             autoUpload
@@ -199,7 +199,7 @@ class Settings extends Component {
           <Button
             variant="outlined"
             color="primary"
-            className={"settings__saveButton"}
+            className={'settings__saveButton'}
             onClick={this.onSaveHandler}
           >
             Save
