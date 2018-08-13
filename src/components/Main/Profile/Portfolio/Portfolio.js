4fbb2049aac7d517aae2acc009500fec8b5ad846
truongtony9 <truong99t@gmail.com>
2018-08-13 15:38:07 -0500

    update

--- src/components/Main/Profile/Portfolio/Portfolio.js
+++ src/components/Main/Profile/Portfolio/Portfolio.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import Carousel from "./Carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { Component } from 'react';
+import Carousel from './Carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url1: "https://s3.amazonaws.com/freelancer-userprofilebucket/pug.jpeg",
-      link1: "pug1",
-      url2: "https://s3.amazonaws.com/freelancer-userprofilebucket/Pug1.jpeg",
-      link2: "pug2",
-      url3: "https://s3.amazonaws.com/freelancer-userprofilebucket/Pug3.jpg",
-      link3: "pug3"
+      url1: 'https://s3.amazonaws.com/freelancer-userprofilebucket/pug.jpeg',
+      link1: 'pug1',
+      url2: 'https://s3.amazonaws.com/freelancer-userprofilebucket/Pug1.jpeg',
+      link2: 'pug2',
+      url3: 'https://s3.amazonaws.com/freelancer-userprofilebucket/Pug3.jpg',
+      link3: 'pug3'
     };
   }
 
