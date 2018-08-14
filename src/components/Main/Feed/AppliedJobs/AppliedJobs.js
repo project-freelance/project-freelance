c6a7afd6cf425ee132e637a3d3bd62af011c5597
truongtony9 <truong99t@gmail.com>
2018-08-14 14:08:46 -0500

    appliedjobs workingish

--- src/components/Main/Feed/AppliedJobs/AppliedJobs.js
+++ src/components/Main/Feed/AppliedJobs/AppliedJobs.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getEmployerPosts } from '../../../../ducks/employerReducer';
-import { getFreelancerPosts } from '../../../../ducks/freelancerReducer';
 import { getUser, getUsers } from '../../../../ducks/userReducer';
 import { getFaveJobs } from '../../../../ducks/freelancerReducer';
+import { Link } from 'react-router-dom';
 import Feed from '../Feed';
+import Post from '../Post/Post';
+import Moment from 'react-moment';
+import Button from '@material-ui/core/Button';
+import EmployerPostModal from '../Post/EmployerPostModal/EmployerPostModal';
 
 class AppliedJobs extends Component {
   constructor(props) {
@@ -13,7 +17,6 @@ class AppliedJobs extends Component {
 
   componentDidMount() {
     this.props.getEmployerPosts();
-    // this.props.getFreelancerPosts();
     this.props.getUsers().then(result => {
       this.setState({
         users: result.value.data
@@ -22,34 +25,121 @@ class AppliedJobs extends Component {
     this.props.getFaveJobs &&
       this.props.getFaveJobs(this.props.user[0] && this.props.user[0].id);
   }
-  render() {
-    // let freelancerAppliedJobs = this.props.favJobs.map(job => {
-    //   return <div key={job.id} />;
-    // });
 
-    let freelancerAppliedJobs = this.props.favJobs.map(job => {
-      if (this.props.favJobs.id === this.props.employerPosts.id) {
-        return <div>{this.props.employerPosts}</div>;
-      } else {
-        return <div>Currently no applied jobs.</div>;
-      }
+  render() {
+    let { employerPosts, isLoading, users } = this.props;
+
+    let matchJob = this.props.favJobs
+      .filter(person => person.freelancer_id === this.props.user[0].id)
+      .map(item => item.employer_post_id);
+
+    let appliedJobs = employerPosts.map((post, index) => {
+      //matching post to user who posted to display user data
+      let postUser = users.map((user, i) => {
+        if (post.user_id == user.id && matchJob.includes(post.id)) {
+          return (
+            <div className="feed__mergedEmployerContainer">
+              <div className="feed__employerData">
+                <Link
+                  className="feed__linkToUser"
+                  to={`/main/profile/${user.id}`}
+                  style={{ textDecoration: 'none' }}
+                >
+                  <div className="feed__employerImage">
+                    <img
+                      src={user.profile_image}
+                      alt="person"
+                      style={{
+                        width: '80px',
+                        height: '80px',
+                        borderRadius: '50%'
+                      }}
+                    />
+                  </div>
+                  <div className="feed__employerName">
+                    <p>{`${user.first_name} ${user.last_name}`}</p>
+                    <p>{user.specialty}</p>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="feed__employerPosting">
+                <h3>Employer Posting</h3>
+                <p>
+                  Post Title:
+                  {post.title}
+                </p>
+                <p>
+                  Job Title:
+                  {post.specialty}
+                </p>
+                <p>
+                  Post Body:
+                  {post.body}
+                </p>
+                <p>Pay: {post.price}</p>
+                <div>
+                  <Moment fromNow>{post.moment}</Moment>
+                </div>
+              </div>
+              <div className="feed__employerModalButton">
+                <Button
+                  style={{
+                    backgroundColor: 'rgb(127, 196, 253)'
+                  }}
+                >
+                  <EmployerPostModal userId={post.user_id} postId={post.id} />
+                </Button>
+                {matchJob.includes(post.id) && (
+                  <div className="feed__applied">
+                    <p>APPLIED</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      });
+      return <div>{postUser} </div>;
     });
 
-    // let this.props.favJobs.id === this.props.employerPosts.id ? {}
-
-    console.log(freelancerAppliedJobs);
-    console.log(this.props);
-    console.log(this.props.user[0]);
-    console.log(this.props.favJobs);
-    console.log(this.props.employerPosts);
-    console.log(freelancerAppliedJobs);
-
-    // console.log(matchJob);
-
+    //render return the merged mapped arrays
     return (
-      <div>
-        <div className="">Test</div>
-        {/* <div className="">{freelancerAppliedJobs}</div> */}
+      <div className="feed__container">
+        <div className="feed__topNav">
+          <Button
+            style={{
+              color: 'white'
+            }}
+            onClick={() => this.filterFreelancers()}
+          >
+            Show Employers Only
+          </Button>
+          <Button
+            style={{
+              color: 'white'
+            }}
+            onClick={() => this.filterEmployers()}
+          >
+            Show Freelancers Only
+          </Button>
+
+          <Button
+            style={{
+              color: 'white'
+            }}
+            onClick={() => this.resetFeed()}
+          >
+            Reset Feed
+          </Button>
+        </div>
+        <div>
+          <h1>In the Feed...</h1>
+          <Post />
+          {appliedJobs}
+        </div>
       </div>
     );
   }
