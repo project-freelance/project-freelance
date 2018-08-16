4f822dfe6c49dd3b75259201007033c9e2ea5f22
JonathanWaller <waller.j27@gmail.com>
2018-08-16 14:39:06 -0500

    more work on freelancers search page

--- src/components/Main/Profile/FreelancersList/FreelancersList.js
+++ src/components/Main/Profile/FreelancersList/FreelancersList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getAllFreelancerInfo } from "../../../../ducks/freelancerReducer";
 import "./FreelancersList.css";
+import { Link } from "react-router-dom";
 
 class FreelancersList extends Component {
   constructor() {
@@ -15,9 +16,11 @@ class FreelancersList extends Component {
   render() {
     console.log(this.props);
     let freelancers = this.props.allFreelancers.map(freelancer => {
+      console.log(freelancer);
       return (
-        <div key={freelancer.id} className="freelancersList__wrapper">
-          {/* <div className="freelancersList__block">
+        <Link to={`/main/profile/${freelancer.user_id}`}>
+          <div key={freelancer.id} className="freelancersList__wrapper">
+            {/* <div className="freelancersList__block">
             <img
               className="freelancersList__img"
               src={freelancer.profile_image}
@@ -25,41 +28,46 @@ class FreelancersList extends Component {
             <div className="freelancersList__name">
               {freelancer.first_name} {freelancer.last_name}
             </div> */}
-          {/* <div>{freelancer.specialty}</div> */}
-          {/* <div>{freelancer.heading}</div> */}
-          {/* <div>{freelancer.skill}</div> */}
-          {/* <div>{freelancer.city}</div> */}
-          {/* </div> */}
-          <div className="freelancersList__popout">
-            {/* <div>Howdy</div> */}
-            <div className="freelancersList__popout__block">
-              <div className="freelancersList__popout__heading">
-                {freelancer.heading}
-              </div>
-              <img
-                id="freelancersList__popout__lineSpace"
-                className="freelancersList__popout__img"
-                src={freelancer.profile_image}
-              />
-              <div
-                id="freelancersList__popout__lineSpace"
-                className="freelancersList__name"
-              >
-                {freelancer.first_name} {freelancer.last_name}
-              </div>
-              <div id="freelancersList__popout__lineSpace">
-                {freelancer.specialty}
-              </div>
 
-              <div id="freelancersList__popout__lineSpace">
-                {freelancer.city}, {freelancer.state}
-              </div>
-              <div id="freelancersList__popout__lineSpace">
-                {freelancer.skills}
+            <div className="freelancersList__popout">
+              <div className="freelancersList__popout__block">
+                <div className="freelancersList__popout__heading">
+                  {freelancer.heading}
+                </div>
+                <div className="freelancersList__popout__belowHeading">
+                  <img
+                    id="freelancersList__popout__lineSpace"
+                    className="freelancersList__popout__img"
+                    src={freelancer.profile_image}
+                  />
+                  <div className="freelancersList__popout__textBlock">
+                    <div
+                      id="freelancersList__popout__lineSpace"
+                      className="freelancersList__name"
+                    >
+                      <div id="freelancer__popout__textBlock__nameRole">
+                        <div className="freelancersList__popout__textBlock__nameState">
+                          {freelancer.first_name} {freelancer.last_name}
+                        </div>
+                      </div>
+                      <div id="freelancer__popout__textBlock__citySkills">
+                        {freelancer.city}, {freelancer.state}
+                      </div>
+                    </div>
+                    <div className="freelancersList__popout__textBlock__specialtySkills">
+                      <div id="freelancer__popout__textBlock__nameRole">
+                        <div>{freelancer.specialty}</div>
+                      </div>
+                      <div id="freelancer__popout__textBlock__citySkills">
+                        {freelancer.skills}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       );
     });
     return (
