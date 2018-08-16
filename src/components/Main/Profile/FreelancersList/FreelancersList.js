f07fecbb9390fd2a17421ab84906da4761e15414
JonathanWaller <waller.j27@gmail.com>
2018-08-16 12:32:14 -0500

    working on freelancers search

--- src/components/Main/Profile/FreelancersList/FreelancersList.js
+++ src/components/Main/Profile/FreelancersList/FreelancersList.js
@@ -16,20 +16,48 @@ class FreelancersList extends Component {
     console.log(this.props);
     let freelancers = this.props.allFreelancers.map(freelancer => {
       return (
-        <div key={freelancer.id}>
-          <div className="freelancersList__block">
+        <div key={freelancer.id} className="freelancersList__wrapper">
+          {/* <div className="freelancersList__block">
             <img
               className="freelancersList__img"
               src={freelancer.profile_image}
             />
             <div className="freelancersList__name">
               {freelancer.first_name} {freelancer.last_name}
+            </div> */}
+          {/* <div>{freelancer.specialty}</div> */}
+          {/* <div>{freelancer.heading}</div> */}
+          {/* <div>{freelancer.skill}</div> */}
+          {/* <div>{freelancer.city}</div> */}
+          {/* </div> */}
+          <div className="freelancersList__popout">
+            {/* <div>Howdy</div> */}
+            <div className="freelancersList__popout__block">
+              <div className="freelancersList__popout__heading">
+                {freelancer.heading}
+              </div>
+              <img
+                id="freelancersList__popout__lineSpace"
+                className="freelancersList__popout__img"
+                src={freelancer.profile_image}
+              />
+              <div
+                id="freelancersList__popout__lineSpace"
+                className="freelancersList__name"
+              >
+                {freelancer.first_name} {freelancer.last_name}
+              </div>
+              <div id="freelancersList__popout__lineSpace">
+                {freelancer.specialty}
+              </div>
+
+              <div id="freelancersList__popout__lineSpace">
+                {freelancer.city}, {freelancer.state}
+              </div>
+              <div id="freelancersList__popout__lineSpace">
+                {freelancer.skills}
+              </div>
             </div>
-            <div>{freelancer.specialty}</div>
-            <div>{freelancer.heading}</div>
-            <div>{freelancer.skill}</div>
-            <div>{freelancer.city}</div>
-            {/* <div className="howdy">howdy</div> */}
           </div>
         </div>
       );
