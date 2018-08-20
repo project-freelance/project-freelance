7453df04b279e8ede924182ee57b079d6c198837
Dylan Brandenburg <brandenburgd@Gmail.com>
2018-08-20 09:04:49 -0500

    worked on styling

--- src/components/Splash/Logo/Logo.js
+++ src/components/Splash/Logo/Logo.js
@@ -22,7 +22,27 @@ class Logo extends Component {
           </div>
         </div>
         <div className="logo__about">
-          <h1>Hello</h1>
+          <div className="logo__about__content">
+            <h1>About Freelancer</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+              pretium risus non metus tincidunt, ac rhoncus quam malesuada.
+              Vivamus imperdiet urna leo, eu pharetra mi porta nec. Aenean
+              convallis iaculis enim sed varius. In ultricies neque et
+              sollicitudin feugiat. Mauris posuere tortor nisi, at tincidunt
+              libero auctor vel. Nam pretium urna at erat lobortis, vitae
+              hendrerit felis pellentesque. Fusce rhoncus mattis blandit.
+              Vestibulum eu mi lobortis, porta sapien in, porta orci. Nulla
+              dignissim est eu quam gravida mattis. Proin semper, nisi congue
+              sollicitudin aliquam, sem est pulvinar turpis, ut maximus turpis
+              nisi eu quam. Nullam vehicula aliquet vestibulum. In at tempus ex.
+              Sed blandit lacus a mollis pharetra. Nullam bibendum rhoncus
+              pretium. Donec tincidunt, dui eu convallis auctor, velit purus
+              blandit est, eu aliquet enim orci non magna. Vestibulum sit amet
+              felis quis quam ullamcorper scelerisque vel sed urna. Aliquam
+              mattis dui in nisl congue, id porta arcu dignissim.
+            </p>
+          </div>
         </div>
       </div>
     );
